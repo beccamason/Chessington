4e597996a9bbaa031b2507e43727d1db6999e6d1
beccamason <beccamason28@gmail.com>
2021-12-02 14:13:09 +0000

    06 Knight Tests Passing

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,57 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            int x = currentPosition.Row;
+            int y = currentPosition.Col;
+
+            if (x + 2 <= 7)
+            {
+                if (y + 1 <= 7)
+                {
+                    availableSquares.Add(Square.At(x + 2, y + 1));
+                }
+                if (y - 1 >= 0)
+                {
+                    availableSquares.Add(Square.At(x + 2, y - 1));
+                }
+            }
+            if (x + 1 <= 7)
+            {
+                if (y + 2 <= 7)
+                {
+                    availableSquares.Add(Square.At(x + 1, y + 2));
+                }
+                if (y - 2 >= 0)
+                {
+                    availableSquares.Add(Square.At(x + 1, y - 2));
+                }
+            }
+            if (x - 2 >= 0)
+            {
+                if (y + 1 <= 7)
+                {
+                    availableSquares.Add(Square.At(x - 2, y + 1));
+                }
+                if (y - 1 >= 0)
+                {
+                    availableSquares.Add(Square.At(x - 2, y - 1));
+                }
+            }
+            if (x - 1 >= 0)
+            {
+                if (y + 2 <= 7)
+                {
+                    availableSquares.Add(Square.At(x - 1, y + 2));
+                }
+                if (y - 2 >= 0)
+                {
+                    availableSquares.Add(Square.At(x - 1, y - 2));
+                }
+            }
+            return availableSquares;
         }
     }
 }
